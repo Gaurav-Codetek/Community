7769486916e7c6f6b7cdfddf78677f9f8f7383c1
Gaurav Patel <gp43883@gmail.com>
2023-11-05 00:09:12 +0530

    Added some animation

--- src/Components/MastSection.js
+++ src/Components/MastSection.js
@@ -4,16 +4,25 @@ import { GoDotFill } from 'react-icons/go'
 import Connect from './Images/connect.png'
 // import shade from './Images/shade_bg.png'
 import ConnLine from './Images/Untitled_design__45_-removebg-preview.png'
+import Moon from './Images/Moon-removebg-preview.png'
+import Sat from './Images/satellite.png'
 
 function MastSection() {
 
-    window.addEventListener("load", () => {
-        document.getElementById("line").style.width = "100%";
-    })
+    // window.addEventListener("load", () => {
+    //     document.getElementById("line").style.width = "100%";
+    // })
 
     return (
         <>
-            <div className=" relative h-full w-full px-8" >
+
+            <div className=" absolute -top-40 -left-24 rounded-full -z-50">
+                <img src={Moon} alt="Moon" className='h-96 w-96' />
+            </div>
+            <div className=" ">
+                <img src={Sat} alt="Satellite" className=' absolute  right-44 top-44 h-14 w-14 -rotate-45 animate-sat' />
+            </div>
+            <div className=" relative h-full w-full px-8 mt-32" >
                 <div className=" w-full text-white h-5/6">
                     <div className="absolute top-20 left-5 h-36 w-10">
                         <img src={ConnLine} className='' alt="Connect-Line" />
@@ -24,15 +33,15 @@ function MastSection() {
                     <div className="absolute top-30 left-4 rounded-full bg-indigo-600 drop-shadow-man-lg p-1 animate-fade">
                         <VscSymbolNamespace className=' fill-current' size="25" />
                     </div>
-                    <div className="bg-gray-800/20 -z-10 backdrop-blur-sm text-left animate-fade-tb p-3 absolute top-14 left-24 rounded-md">
-                        <h1 className="text-cyan-400"><b>CodersTek</b> Community</h1>
-                        <p className="text-xs flex items-center"><GoDotFill color='rgb(34 197 94)' className="mt-px mr-px" />backed by<b className="text-green-500 p-px ml-0.5"> DIC</b></p>
+                    <div className="bg-gray-800/20 -z-10 backdrop-blur-sm text-left animate-fade-tb p-4 absolute top-12 left-24 rounded-md">
+                        <h1 className="text-cyan-400 text-xl"><b>CodersTek</b> Community</h1>
+                        <p className="text-base flex items-center"><GoDotFill color='rgb(34 197 94)' className="mt-0.5 mr-px" />backed by<b className="text-green-500 p-px ml-0.5"> DIC</b></p>
                     </div>
 
                     <div className="flex-col absolute top-40 h-full">
-                        <h1 className="text-left text-6xl font-semibold pl-8 animate-fadeOnly mb-4">Let's start from here</h1>
-                        <h1 className="text-slate-500 text-left pl-8 text-3xl mb-8 animate-fadeOnly">One-stop Platform to surf various<b className='ml-2 text-cyan-400'>PROJECTS</b></h1>
-                        <input type="text" name="email" id="email" placeholder='Email address' className="animate-fade-seq-1 w-4/5 mb-3 py-2 rounded-md text-black border-2 focus:border-indigo-600 outline-none px-2"/>
+                        <h1 className="text-left text-55xl font-semibold mr-14 pl-8 animate-fadeOnly mb-4">Let's start from here</h1>
+                        <h1 className="text-slate-500 text-left pl-8 mr-16 text-2xl mb-8 animate-fadeOnly">One-stop Platform to surf various<b className='ml-2 text-cyan-400'>PROJECTS</b></h1>
+                        <input type="text" name="email" id="email" placeholder='Email address' className="animate-fade-seq-1 w-4/5 mb-3 py-2 rounded-md text-black border-2 focus:border-indigo-600 outline-none px-2" />
                         <input type="submit" value="Sign up for CodersTek Community" className="animate-fade-seq-2 bg-indigo-900 hover:bg-indigo-950 py-3 font-bold w-4/5 rounded-md" />
                     </div>
                 </div>
@@ -40,8 +49,8 @@ function MastSection() {
                 <div className="hidden bg-white w-2/5">
                     dads
                 </div>
-                <div className="w-4/5 transition-2 left-8 absolute top-44">
-                    <div id="line" className="w-1 bg-gradient-to-r from-indigo-600 from-50% via-indigo-500 via-80% to-indigo-300  p-px  rounded-sm rotate-90 origin-left transition-2">
+                <div className="w-full transition-2 left-8 absolute top-44">
+                    <div id="line" className="w-full animate-line bg-gradient-to-r from-indigo-600 from-50% via-indigo-500 via-80% to-indigo-300  p-px  rounded-sm rotate-90 origin-left transition-2">
                         {/* <hr id="line" className="w-10 border-y-2 border-violet-400 rotate-90 origin-left transition-2"/> */}
                     </div>
                 </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,8 +10,8 @@ function Navbar() {
                 <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                     <div className="relative flex h-16 items-center justify-between">
                         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                            <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white aria-pressed:block" onClick={() => setIsOpen((prev) => !prev)} aria-controls="mobile-menu" aria-expanded="false">
-                                <span className="absolute -inset-0.5"></span>
+                            <button type="button" className="bg-gray-700 relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white aria-pressed:block" onClick={() => setIsOpen((prev) => !prev)} aria-controls="mobile-menu" aria-expanded="false">
+                                <span className="absolute -inset-0.5 "></span>
                                 <span className="sr-only">Open main menu</span>
                                 <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
