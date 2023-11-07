340db0b540eef0164ebcbd2ccf693672faf4c0db
Gaurav Patel <gp43883@gmail.com>
2023-11-07 23:12:30 +0530

    Added onScroll Animation using IntersectionObserver

--- src/Components/Partner.js
+++ src/Components/Partner.js
@@ -7,31 +7,50 @@ function Partner() {
     const [isIntersecting, setIsIntersecting] = useState(false);
     const ref = useRef(null);
     const refLine = useRef(null);
+    const refPartner = useRef(null);
 
 
     useEffect(() => {
         // let a = document.getElementsByClassName('section')
         const observer = new IntersectionObserver(([entry]) => {
             setIsIntersecting(entry.isIntersecting);
-        }, { rootMargin: "0px", threshold: .5 });
+            console.log(isIntersecting)
+        }, { rootMargin:"-180px", threshold: 1});
         observer.observe(ref.current);
 
-        const observerLine = new IntersectionObserver(([entry]) => {
-            setIsIntersecting(entry.isIntersecting);
-        }, { rootMargin: "0px", threshold: .5 });
-        observerLine.observe(refLine.current);
-    }, [])
+        // const observerLine = new IntersectionObserver(([entry]) => {
+        //     setIsIntersecting(entry.isIntersecting);
+        // }, { rootMargin: "0px", threshold: .5 });
+        // observerLine.observe(refLine.current);
+
+        // const observerPartner = new IntersectionObserver(([entry]) => {
+        //     setIsIntersecting(entry.isIntersecting);
+        // }, { rootMargin: "0px", threshold: .5 });
+        // observerPartner.observe(refPartner.current);
+    }, [isIntersecting])
 
     useEffect(() => {
         if (isIntersecting) {
             ref.current.querySelectorAll("div").forEach((el) => {
                 el.classList.add("animate-gen")
+                el.classList.add("block")
+                el.classList.remove("hidden")
             });
         }
 
         if (isIntersecting) {
             refLine.current.querySelectorAll("div").forEach((el) => {
                 el.classList.add("animate-line")
+                el.classList.add("block")
+                el.classList.remove("hidden")
+            })
+        }
+
+        if (isIntersecting) {
+            refPartner.current.querySelectorAll("div").forEach((el) => {
+                el.classList.add("animate-partner")
+                el.classList.add("block")
+                el.classList.remove("hidden")
             })
         }
     }, [isIntersecting])
@@ -41,22 +60,25 @@ function Partner() {
     return (
         <>
             <div className="relative h-full mt-0">
-                <div className="text-white absolute top-24 left-14 text-lg">
+                <div ref={refPartner} className="">
+                <div className="hidden text-white absolute top-24 left-14 text-lg">
                     <h1>Our Partners</h1>
                 </div>
-                <div className=" absolute top-24 left-4 bg-lime-500 rounded-full drop-shadow-man-lg-part p-1">
-                    <AiOutlineApartment color="white" size="25" />
                 </div>
-                <div ref={ref} className="section">
-                    <div className="absolute top-40 left-14">
+                <div ref={ref}>
+                    <div className=" hidden absolute top-24 left-4 bg-lime-500 rounded-full drop-shadow-man-lg-part p-1">
+                        <AiOutlineApartment color="white" size="25" />
+                    </div>
+
+                    <div className="hidden absolute top-40 left-14">
                         <img src={Dic} alt="DIC" className="h-28 w-28" />
                     </div>
-                    <div className="absolute top-40 left-56">
+                    <div id="section" className=" hidden sec absolute top-40 left-56">
                         <img src={uiet} alt="UIET" className="h-28 w-28" />
                     </div>
                 </div>
                 <div ref={refLine} className="">
-                    <div className="w-full left-8 absolute top-36 bg-gradient-to-r from-lime-600 from-50% via-lime-500 via-80% to-lime-300  p-px  rounded-sm rotate-90 origin-left transition-2">
+                    <div className="hidden w-full left-8 absolute top-36 bg-gradient-to-r from-lime-600 from-50% via-lime-500 via-80% to-lime-300  p-px  rounded-sm rotate-90 origin-left transition-2">
                     </div>
                 </div>
             </div>
